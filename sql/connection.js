a8ad70a1d0f0dbe5e97043941b2ee80f2f7fafba
jbryans <jbryans2014@gmail.com>
2022-07-21 21:11:59 -0500

    finished

--- sql/connection.js
+++ sql/connection.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql')
 
+
+// const {DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_DATABASE} = process.env
+
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
+        host: '',
+        user: '',
+        password: '',
+        database: '',
+        port: 
       })
 
       return this.pool
